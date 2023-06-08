e28510eacd0fbb5af2bce03138de9674910d1ca8
Razaklekan113 <razaklekan11@gmail.com>
2023-06-09 00:04:17 +0100

    updated this file

--- ecommerce/app/views.py
+++ ecommerce/app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
 
 def home(request):
     return render(request, "app/home.html")
+
+class CategoryView(View):
+    def get(self, request, val):
+        return render(request, "app/category.html")
