8bf01236034bb05538599e4c615ecf7fa24a6425
Razaklekan113 <razaklekan11@gmail.com>
2023-06-09 01:18:59 +0100

    updated this file

--- ecommerce/app/views.py
+++ ecommerce/app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views import View
-
+from .models import Product
+from django.db.models import Count
 # Create your views here.
 
 def home(request):
@@ -9,4 +9,6 @@ def home(request):
 
 class CategoryView(View):
     def get(self, request, val):
-        return render(request, "app/category.html")
+        product = Product.objects.filter(category=val)
+        title = Product.objects.filter(category=val).values('title')
+        return render(request, "app/category.html",locals())
