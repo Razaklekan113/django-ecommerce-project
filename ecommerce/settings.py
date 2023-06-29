c6898f0b2524a72ef55814a75bd1631df85dab86
Razaklekan113 <razaklekan11@gmail.com>
2023-06-29 03:28:41 +0100

    committed to this file

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'staticfiles', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
