1577cbac2dd71642dd953b201a71dc8726cab7d0
Razaklekan113 <razaklekan11@gmail.com>
2023-06-29 03:41:06 +0100

    committed to this file

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -122,7 +122,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
 LOGIN_REDIRECT_URL = '/profile/'
 STATICFILES_DIRS = [BASE_DIR / "app" / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
